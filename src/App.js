2aa89de98126f72f8ccd35a5a69cc98baec337d2
Jun-Araki <jun7772006@gmail.com>
2026-02-05 21:44:10 +0900

    complete pokemon app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getAllPokemon, getPokemon } from './utls/pokemon';
+import Card from './components/card/Card';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      loadPokemon(res.results);
+      setNextURL(res.next);
+      // setPrevURL(res.previous);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async(data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      }))
+    setPokemonData(_pokemonData);
+  }
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
   return (
     <div className="App">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon}></Card>;
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>Prev</button>
+              <button onClick={handleNextPage}>Next</button>
+            </div>
+          </>
+      )}
     </div>
   );
 }
